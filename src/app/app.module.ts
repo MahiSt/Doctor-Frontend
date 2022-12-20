17d7128de8cb438af7ad7f8b4f74dd6dbe17102b
Mahi <abmahistk@gmail.com>
2022-12-20 22:55:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DoctorListComponent } from './components/doctor-list/doctor-list.component';
+import { DoctorDetailsComponent } from './components/doctor-details/doctor-details.component';
+import { AddDoctorComponent } from './components/add-doctor/add-doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavBarComponent,
+    DoctorListComponent,
+    DoctorDetailsComponent,
+    AddDoctorComponent
   ],
   imports: [
     BrowserModule,
